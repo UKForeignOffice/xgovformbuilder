d4155b1ee3cc7daedf7a7a04c62642f0ab692480
Trevor Rea <trevorr@kainos.com>
2020-12-23 15:53:46 +0000

    Correct CSP policy

--- runner/src/server/plugins/blankie.ts
+++ runner/src/server/plugins/blankie.ts
@@ -11,10 +11,15 @@ export const configureBlankiePlugin = (
   return {
     plugin: Blankie,
     options: {
+      connectSrc: (() =>
+        ["self"].concat(
+          config.matomoUrl ? [config.matomoUrl] : []
+        ))(),
+      defaultSrc: ["self"],
       fontSrc: ["self", "data:"],
       scriptSrc: (() =>
-        ["self", "unsafe-inline"].concat(
-          config.matomoUrl ? [config.matomoUrl] : []
+        ["self"].concat(
+          config.matomoUrl ? [config.matomoUrl/piwik/piwik.js] : []
         ))(),
       styleSrc: ["self", "unsafe-inline"],
       imgSrc: (() =>
